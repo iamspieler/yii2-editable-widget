fd728e6a94f7b7a4c718ba75d7698700edaf2923
iamspieler <iamspieler@users.noreply.github.com>
2022-08-21 17:13:00 +0300

    Update EditableBootstrapAsset.php

--- src/EditableBootstrapAsset.php
+++ src/EditableBootstrapAsset.php
@@ -30,6 +30,6 @@ class EditableBootstrapAsset extends AssetBundle
 
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapPluginAsset',
+        'yii\bootstrap5\BootstrapPluginAsset',
     ];
 }
